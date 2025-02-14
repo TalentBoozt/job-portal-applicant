6eccf7ac0108eb55ebd668b1a42c157651ef1dde
kavi castelo <kavindu123450@gmail.com>
2025-02-15 03:03:47 +0530

    update server

--- server.ts
+++ server.ts
@@ -5,10 +5,10 @@ import { CommonEngine } from '@angular/ssr';
 import * as express from 'express';
 import { existsSync } from 'node:fs';
 import { join } from 'node:path';
-import {AppServerModule} from './src/main.server';
+import { AppServerModule } from './src/main.server';
 import { Request, Response, NextFunction } from 'express';
 
-// The Express app is exported so that it can be used by serverless Functions.
+// The Express app is exported so that it can be used by serverless functions.
 export function app(): express.Express {
   const server = express();
   const distFolder = join(process.cwd(), 'dist/client/browser');
@@ -21,24 +21,18 @@ export function app(): express.Express {
   server.set('view engine', 'html');
   server.set('views', distFolder);
 
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
-  server.get('*.*', express.static(distFolder, {
-    maxAge: '1y'
-  }));
+  // Serve static files
+  server.use(express.static(distFolder, { maxAge: '1y' }));
 
-  // All regular routes use the Angular engine
+  // SSR route handling
   server.get('*', (req: Request, res: Response, next: NextFunction) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
-
     commonEngine
       .render({
         bootstrap: AppServerModule,
         documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
+        url: req.protocol + '://' + req.get('host') + req.originalUrl,
         publicPath: distFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+        providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
@@ -49,22 +43,20 @@ export function app(): express.Express {
 
 function run(): void {
   const port = process.env['PORT'] || 4000;
-
-  // Start up the Node server
   const server = app();
+
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
 
-// Webpack will replace 'require' with '__webpack_require__'
-// '__non_webpack_require__' is a proxy to Node 'require'
-// The below code is to ensure that the server is run only when not requiring the bundle.
+// Run the server if it's executed directly
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
-const moduleFilename = mainModule && mainModule.filename || '';
+const moduleFilename = mainModule?.filename || '';
+
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
 }
 
-export default AppServerModule;
+export default app;
